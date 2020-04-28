d246d823731c7412f028176e9078b7c33c961d81
github username <github email address>
2020-04-28 11:50:02 -0600

    db:migrate

--- hot_spring_api/db/migrate/20200428173703_create_recoveries.rb
+++ hot_spring_api/db/migrate/20200428173703_create_recoveries.rb
@@ -2,7 +2,7 @@ class CreateRecoveries < ActiveRecord::Migration[6.0]
   def change
     create_table :recoveries do |t|
       t.string :activity
-      t.interger :activity_level
+      t.integer :activity_level
       t.float :cost
       t.references :location, null: false, foreign_key: true
 
