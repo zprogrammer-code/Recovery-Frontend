8819fdb6527d11e4178ed4ba9f195cd0281cb1fb
github username <github email address>
2020-04-28 11:47:20 -0600

    migration

--- hot_spring_api/db/migrate/20200428173337_create_relaxes.rb
+++ hot_spring_api/db/migrate/20200428173337_create_relaxes.rb
@@ -2,7 +2,7 @@ class CreateRelaxes < ActiveRecord::Migration[6.0]
   def change
     create_table :relaxes do |t|
       t.string :name
-      t.integer :capcity
+      t.integer :capacity
       t.string :vibe
       t.references :location, null: false, foreign_key: true
 
