9ffa368757ce4a76751dd17435e27036a40d7f37
alohajt <alohajiatao@gmail.com>
2020-04-28 14:32:42 -0600

    jt 1

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,7 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "200px";
   }
   
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
