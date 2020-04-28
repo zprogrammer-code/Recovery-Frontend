b938096b053f709e889e36dcfcf7d4d251537662
Jeremy <jerome215@comcast.net>
2020-04-28 14:18:00 -0600

    Built the Location and Recoveries controllers

--- hot_spring_api/config/routes.rb
+++ hot_spring_api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :recoveries
-  resources :rests
-  resources :relaxes
-  resources :locations
+  resources :recoveries, only: [:index, :create, :show]
+  resources :rests, only: [:index, :create, :show]
+  resources :relaxes, only: [:index, :create, :show]
+  resources :locations, only: [:index, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
